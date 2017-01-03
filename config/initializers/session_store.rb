6dbb4b5f843bd00af3ed980408e00b4ae4a11ada
Arakawa Shintaro <arashinbe@gmail.com>
2017-01-03 18:36:44 +0900

    bundle update rails5

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_chat-with-rails5ver_session'
+Rails.application.config.session_store :cookie_store, key: '_chat_with_rails5ver_session'
